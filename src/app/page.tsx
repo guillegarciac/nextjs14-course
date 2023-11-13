b670f8ce3fd47478eab16ee3bf681f13d7c1aba7
guille garcia <ggarciacardiel@gmail.com>
2023-11-13 19:49:22 +0100

    image components next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 
 import Header from "@/components/Header";
 import Logo from "@/components/Header/Logo";
+import Image from "next/image";
+import background from "../../public/background.jpg";
+import vercel from "../../public/vercel.svg";
 
 export default function Home() {
   const name = 'world';
@@ -12,6 +15,15 @@ export default function Home() {
         <Logo />
       </Header>
       hello world from docker container
+      <div /* className="w-[800px]" */>
+        <Image quality= {100} src={background} alt="Background Logo" />
+        <Image 
+          src={vercel} 
+          width={360}
+          height={360}
+          alt="vercel Logo"
+        />
+      </div>
     </main>
   )
 }
