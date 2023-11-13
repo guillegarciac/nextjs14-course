094f9a88730b758ad94061256a29a45deee371e4
guille garcia <ggarciacardiel@gmail.com>
2023-11-13 10:35:52 +0100

    props.children example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   const name = 'world';
   return (
     <main>
-      <Header name={name} age={35} isMan={true}/>
+      <Header name={name} age={35} isMan={true}>
+        <p>A message from Home</p>
+      </Header>
       hello world from docker container
     </main>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,6 +10,7 @@ export default function Header(props: any) {
     <header className="bg-white text-red-600 p-4 flex justify-between items-center">
       <Logo />
       <Menu {...props}/>
+      {props.children}
     </header>
   )
 }
